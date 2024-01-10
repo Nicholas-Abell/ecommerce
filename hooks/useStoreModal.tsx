eda10b9b268ed2477eb5864b2ee73b5795bb7433
Nick <darkforceyoda@gmail.com>
2024-01-10 11:00:47 -0600

    useStoreModal hook

---
+++ hooks/useStoreModal.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { create } from "zustand";
+
+type useStoreModalProps = {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+};
+
+export const useStoreModal = create<useStoreModalProps>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
