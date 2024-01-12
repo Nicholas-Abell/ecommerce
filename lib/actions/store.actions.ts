2c5c32e20fce776ca4ac2ddcc13f3b195f1711de
Nick <darkforceyoda@gmail.com>
2024-01-12 16:49:18 -0600

    store server actions

---
+++ lib/actions/store.actions.ts
@@ -0,0 +1,12 @@
+"use server";
+import prisma from "../prisma/prisma";
+
+// same as /api/store route.ts
+// export async function CreateStore(name: string) {
+//   const store = await prisma.store.create({
+//     data: {
+//       name,
+//       userId: "testid",
+//     },
+//   });
+// }
