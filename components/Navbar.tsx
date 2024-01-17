018a8fbdea481da50b41fe3971fd6fdb41299212
Nick <darkforceyoda@gmail.com>
2024-01-16 20:21:42 -0600

    created navbar comp

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
-type NavbarProps = {
-    
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div className="border-b">
+      <div className="flex items-center px-4 h-16">Store Switcher</div>
+      <div>Routes</div>
+      <div className="ml-auto flex items-center space-x-4">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
 };
-
-const Navbar:React.FC<NavbarProps> = () => {
-    
-    return <div>Have a good coding</div>
-}
 export default Navbar;
