d48eeaff3f412c2cc40021e15b903b1e54b2fdfc
Nick <darkforceyoda@gmail.com>
2024-01-19 22:50:54 -0600

    code cleanup

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import StoreModal from "../modal/StoreModal";
 
 type ModalProviderProps = {};
