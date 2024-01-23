529a72c0e0858eda83bbfd0e584743e9094633ca
Nick <darkforceyoda@gmail.com>
2024-01-23 12:52:09 -0600

    DesktopNav routes/styles

--- components/navigation/DesktopNav.tsx
+++ components/navigation/DesktopNav.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import { useParams, usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
-type DesktopNavProps = {
-    
+type DesktopNavProps = {};
+
+const DesktopNav: React.FC<DesktopNavProps> = () => {
+  const pathname = usePathname();
+  const params = useParams();
+
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
+
+  return (
+    <div className="flex items-center space-x-4 lg:space-x-">
+      {routes.map((route, index) => (
+        <Link
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary", route.active ? "text-black dark:text-white" : "text-muted-foreground"
+          )}
+          key={index}
+          href={route.href}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </div>
+  );
 };
-
-const DesktopNav:React.FC<DesktopNavProps> = () => {
-    
-    return <div>Have a good coding</div>
-}
 export default DesktopNav;
