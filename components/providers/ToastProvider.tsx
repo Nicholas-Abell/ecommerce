54356b1742d51b9c4aaec033f6300b91bcb970a0
Nick <darkforceyoda@gmail.com>
2024-01-24 10:49:20 -0600

    useEffect to prevent hydration errors

--- components/providers/ToastProvider.tsx
+++ components/providers/ToastProvider.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 
 export const ToastProvider = () => {
+  //prevents hydration errors, particularly when working with local state
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   return <Toaster />;
 };
