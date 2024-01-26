d3540449363258df8e8c03264fcd69d516cc2573
Nick <darkforceyoda@gmail.com>
2024-01-26 09:43:03 -0600

    redirect to sign in, store fetch

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,16 +1,32 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import React from "react";
 import Routes from "./Routes";
+import StoreSwitcher from "./StoreSwitcher";
+import { redirect } from "next/navigation";
 
 type NavbarProps = {};
 
-const Navbar: React.FC<NavbarProps> = () => {
+const Navbar: React.FC<NavbarProps> = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prisma.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b">
-      <div className="flex items-center px-4 h-16">Store Switcher</div>
-      <Routes />
-      <div className="ml-auto flex items-center space-x-4">
-        <UserButton afterSignOutUrl="/" />
+      <div className="flex items-center px-4 h-16 gap-4">
+        <StoreSwitcher items={stores} />
+        <Routes />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
       </div>
     </div>
   );
