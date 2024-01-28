f1cf5c834269278172d07e1c0a0bd3e4fe6d1f59
Nick <darkforceyoda@gmail.com>
2024-01-27 22:45:35 -0600

    redirects on no store/user, store fetch server action

--- app/(dashboard)/[storeId]/settings/page.tsx
+++ app/(dashboard)/[storeId]/settings/page.tsx
@@ -1,8 +1,30 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-type pageProps = {};
+import prisma from "@/lib/prisma/prisma";
 
-const page: React.FC<pageProps> = () => {
+type pageProps = {
+  params: {
+    storeId: string;
+  };
+};
+
+const page: React.FC<pageProps> = async ({ params }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prisma.store.findFirst({
+    where: { id: params.storeId, userId },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
   return <div>Settings</div>;
 };
 export default page;
