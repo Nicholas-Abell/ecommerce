e61e281d8b3722242c317aeed0445cdc2d5e4600
Nick <darkforceyoda@gmail.com>
2024-01-28 23:04:59 -0600

    Styles: SettingsForm

--- components/forms/SettingsForm.tsx
+++ components/forms/SettingsForm.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+"use client";
+
+import React from "react";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+
+import Heading from "../ui/heading";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
 
 type SettingsFormProps = {
-    
+  storeData: Store;
 };
 
-const SettingsForm:React.FC<SettingsFormProps> = () => {
-    
-    return <div>Have a good coding</div>
-}
+const SettingsForm: React.FC<SettingsFormProps> = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage Store Preferences" />
+        <Button variant="destructive" onClick={() => {}} size="icon">
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+};
 export default SettingsForm;
