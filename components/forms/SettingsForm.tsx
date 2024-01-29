724624db8115701f6f56ca8c1d521b80d7b4f69f
Nick <darkforceyoda@gmail.com>
2024-01-29 11:01:01 -0600

    SettingsForm layout

--- components/forms/SettingsForm.tsx
+++ components/forms/SettingsForm.tsx
@@ -1,18 +1,48 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import Heading from "../ui/heading";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
 
 type SettingsFormProps = {
   storeData: Store;
 };
 
-const SettingsForm: React.FC<SettingsFormProps> = () => {
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ storeData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: storeData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -22,6 +52,35 @@ const SettingsForm: React.FC<SettingsFormProps> = () => {
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className=" space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save Changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
