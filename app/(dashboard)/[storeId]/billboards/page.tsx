9c93aabfc112b9524774f871be91a1a9fd6a7252
Nick <darkforceyoda@gmail.com>
2024-02-01 22:53:42 -0600

    billboards page route

---
+++ app/(dashboard)/[storeId]/billboards/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type BillboardsPageProps = {};
+
+const BillboardsPage: React.FC<BillboardsPageProps> = () => {
+  return <div>Billboards</div>;
+};
+export default BillboardsPage;
