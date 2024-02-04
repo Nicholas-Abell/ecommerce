9faf3df1f7dba5afe6884d5ddc2f4e7a42437bd9
Nick <darkforceyoda@gmail.com>
2024-02-03 22:58:31 -0600

    Client comp -> _components

---
+++ app/(dashboard)/[storeId]/billboards/_components/Client.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ClientProps = {
+    
+};
+
+const Client:React.FC<ClientProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default Client;
--- app/(dashboard)/[storeId]/billboards/page.tsx
+++ app/(dashboard)/[storeId]/billboards/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Client from "./_components/Client";
 
 type BillboardsPageProps = {};
 
@@ -6,7 +7,7 @@ const BillboardsPage: React.FC<BillboardsPageProps> = () => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 py-6">
-        {/* <BillBoardClient /> */}
+        <Client />
       </div>
     </div>
   );
