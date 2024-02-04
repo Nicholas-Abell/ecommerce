6e9fa40880720381f1f3e1f7dfa9d9b7c334480e
Nick <darkforceyoda@gmail.com>
2024-02-03 22:53:04 -0600

    BillboardPage layout

--- app/(dashboard)/[storeId]/billboards/page.tsx
+++ app/(dashboard)/[storeId]/billboards/page.tsx
@@ -3,6 +3,12 @@ import React from "react";
 type BillboardsPageProps = {};
 
 const BillboardsPage: React.FC<BillboardsPageProps> = () => {
-  return <div>Billboards</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 py-6">
+        {/* <BillBoardClient /> */}
+      </div>
+    </div>
+  );
 };
 export default BillboardsPage;
