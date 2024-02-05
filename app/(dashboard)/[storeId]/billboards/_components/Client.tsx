37b93973fa059444467a24f8ad0a4fcd1f8f4cec
Nick <darkforceyoda@gmail.com>
2024-02-04 20:03:18 -0600

    Billboard client layout

--- app/(dashboard)/[storeId]/billboards/_components/Client.tsx
+++ app/(dashboard)/[storeId]/billboards/_components/Client.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+"use client";
 
-type ClientProps = {
-    
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import React from "react";
+
+type ClientProps = {};
+
+const Client: React.FC<ClientProps> = () => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Billboards (0)"
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
 };
-
-const Client:React.FC<ClientProps> = () => {
-    
-    return <div>Have a good coding</div>
-}
 export default Client;
