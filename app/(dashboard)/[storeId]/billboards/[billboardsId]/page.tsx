7712b14798c5a6224fe98ecb95cdf10e5e01deb9
Nick <darkforceyoda@gmail.com>
2024-02-04 22:12:16 -0600

    billboardForm created

--- app/(dashboard)/[storeId]/billboards/[billboardsId]/page.tsx
+++ app/(dashboard)/[storeId]/billboards/[billboardsId]/page.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import prisma from "@/lib/prisma/prisma";
+import BillboardForm from "./_components/BillboardForm";
+import BillboardsPage from "../page";
 
 type pageProps = {
   params: { billboardId: string };
 };
 
 const page: React.FC<pageProps> = async ({ params }) => {
-  // const billboard = await prisma.billboard.findUnique({
-  //   where: { id: params.billboardId },
-  // });
+  const billboard = await prisma.billboard.findUnique({
+    where: { id: params.billboardId },
+  });
 
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6"></div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm billboardData={billboard} />
+      </div>
     </div>
   );
 };
