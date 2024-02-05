c3a81d1ff92c242a9566c802369a193e61370b6f
Nick <darkforceyoda@gmail.com>
2024-02-04 20:37:55 -0600

    folder structure: _components folders with comp  unique to route

---
+++ app/(dashboard)/[storeId]/billboards/[billboardsId]/_components/BillboardForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type BillboardFormProps = {
+    
+};
+
+const BillboardForm:React.FC<BillboardFormProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default BillboardForm;
--- app/(dashboard)/[storeId]/billboards/[billboardsId]/page.tsx
+++ app/(dashboard)/[storeId]/billboards/[billboardsId]/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import prisma from "@/lib/prisma/prisma";
 
 type pageProps = {
-    
+  params: { billboardId: string };
 };
 
-const page:React.FC<pageProps> = () => {
-    
-    return <div>Have a good coding</div>
-}
+const page: React.FC<pageProps> = async ({ params }) => {
+  // const billboard = await prisma.billboard.findUnique({
+  //   where: { id: params.billboardId },
+  // });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6"></div>
+    </div>
+  );
+};
 export default page;
