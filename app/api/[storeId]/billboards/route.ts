1ca208586cfe506e7ae3bcda9486b38e91fbd683
Nick <darkforceyoda@gmail.com>
2024-02-06 10:14:54 -0600

    Billboards api route

---
+++ app/api/[storeId]/billboards/route.ts
@@ -0,0 +1,93 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenitcated", { status: 401 });
+    }
+
+    if (!label) {
+      return new NextResponse("Label is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("ImageUrl is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("storeId is required", { status: 400 });
+    }
+
+    const storeByUserId = await prisma.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const billboard = await prisma.billboard.create({
+      data: { label, imageUrl, storeId: params.storeId },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARDS_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("storeId is required", { status: 400 });
+    }
+
+    const billboards = await prisma.billboard.findMany({
+      where: { storeId: params.storeId },
+    });
+
+    return NextResponse.json(billboards);
+  } catch (error) {
+    console.log("[BILLBOARDS_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenitcated", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const store = await prisma.store.deleteMany({
+      where: { id: params.storeId, userId },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[BILLBOARDS_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
