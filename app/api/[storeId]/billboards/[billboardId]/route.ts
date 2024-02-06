6bbf1274d61d59a9ac74f01ac31b8370a235bbfa
Nick <darkforceyoda@gmail.com>
2024-02-06 10:29:47 -0600

    [BILLBOARDS_GET] function

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -1,6 +1,26 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+export async function GET(
+  _req: Request,
+  { params }: { params: { billboardId: string } }
+) {
+  try {
+    if (!params.billboardId) {
+      return new NextResponse("Billboard id is required", { status: 400 });
+    }
+
+    const billboard = await prisma.billboard.findUnique({
+      where: { id: params.billboardId },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { storeId: string; billboardId: string } }
