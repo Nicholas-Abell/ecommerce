96b8d4e3d327476035c81a701c92cb941b83fe68
Nick <darkforceyoda@gmail.com>
2024-03-13 10:47:35 -0500

    sizes column rework

--- app/(dashboard)/[storeId]/sizes/_components/Columns.tsx
+++ app/(dashboard)/[storeId]/sizes/_components/Columns.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import CellAction from "@/app/(dashboard)/[storeId]/billboards/_components/CellAction";
+import CellAction from "../_components/CellAction";
 import { ColumnDef } from "@tanstack/react-table";
 
-export type BillboardColumn = {
+export type SizesColumn = {
   id: string;
-  label: string;
+  name: string;
+  value: string;
   createdAt: string;
 };
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<SizesColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "value",
+    header: "Value",
   },
   {
     accessorKey: "createdAt",
