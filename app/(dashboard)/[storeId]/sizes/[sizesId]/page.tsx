a1b41c2b8e482ab77bb8c3d60d50b980e8912a88
Nick <darkforceyoda@gmail.com>
2024-03-12 23:10:19 -0500

    rename

--- app/(dashboard)/[storeId]/sizes/[sizesId]/page.tsx
+++ app/(dashboard)/[storeId]/sizes/[sizesId]/page.tsx
@@ -2,24 +2,20 @@ import prisma from "@/lib/prisma/prisma";
 
 import { BillboardForm } from "./_components/BillboardForm";
 
-const BillboardPage = async ({
-  params
-}: {
-  params: { billboardId: string }
-}) => {
+const SizesPage = async ({ params }: { params: { billboardId: string } }) => {
   const billboard = await prisma.billboard.findUnique({
     where: {
-      id: params.billboardId
-    }
+      id: params.billboardId,
+    },
   });
 
-  return ( 
+  return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <BillboardForm initialData={billboard} />
       </div>
     </div>
   );
-}
+};
 
-export default BillboardPage;
+export default SizesPage;
