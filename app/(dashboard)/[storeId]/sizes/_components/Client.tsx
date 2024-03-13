5a62f4b146b7e33ba368d357432aad1d9d6f2a57
Nick <darkforceyoda@gmail.com>
2024-03-13 10:58:44 -0500

    Client for Sizes rework

--- app/(dashboard)/[storeId]/sizes/_components/Client.tsx
+++ app/(dashboard)/[storeId]/sizes/_components/Client.tsx
@@ -7,12 +7,12 @@ import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { BillboardColumn, columns } from "./Columns";
+import { SizesColumn, columns } from "./Columns";
 import { DataTable } from "@/components/ui/data-table";
 import ApiList from "@/components/ui/api-list";
 
 type ClientProps = {
-  data: BillboardColumn[];
+  data: SizesColumn[];
 };
 
 const Client: React.FC<ClientProps> = ({ data }) => {
@@ -23,21 +23,19 @@ const Client: React.FC<ClientProps> = ({ data }) => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboards for your store"
+          title={`Sizes (${data.length})`}
+          description="Manage sizes for your store"
         />
-        <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
-        >
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
           <Plus className="mr-2 h-4 w-4" />
           Add New
         </Button>
       </div>
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
-      <Heading title="API" description="API calls for Billboards" />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API calls for Sizes" />
       <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardsId" />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
     </>
   );
 };
